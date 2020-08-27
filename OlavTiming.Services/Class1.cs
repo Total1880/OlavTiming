2c7a527584fcbd41ddd3fd727f4a46514891f755
olav hendrickx <olav.hendrickx@hotmail.com>
2020-08-27 11:13:51 +0200

    Add project files.

---
+++ OlavTiming.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OlavTiming.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ OlavTimingRepositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OlavTiming.Repositories
+{
+    public class Class1
+    {
+    }
+}
