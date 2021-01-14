0f6e8fd8a551d4ef2e02bc1990ae48f1b9bdd2a0
Olav Hendrickx <olav.hendrickx@capgemini.com>
2021-01-14 11:00:39 +0100

    Add Page for overview tasks

--- OlavTiming/MainWindow.xaml.cs
+++ OlavTiming/MainWindow.xaml.cs
@@ -13,20 +13,28 @@ namespace OlavTiming
     {
         private StartScreenPage _startScreenPage;
         private RunningTaskPage _runningTaskPage;
+        private OverviewTasksPage _overviewTasksPage;
 
         public StartScreenPage StartScreenPage => _startScreenPage ??= new StartScreenPage();
         public RunningTaskPage RunningTaskPage => _runningTaskPage ??= new RunningTaskPage();
+        public OverviewTasksPage OverviewTasksPage => _overviewTasksPage ??= new OverviewTasksPage();
 
         public MainWindow()
         {
             InitializeComponent();
             MainWindowFrame.NavigationService.Navigate(StartScreenPage);
             Messenger.Default.Register<OpenRunningTaskPageMessage>(this, OpenNewTask);
+            Messenger.Default.Register<OpenOverviewTaskPageMessage>(this, OpenOverviewTasks);
         }
 
         private void OpenNewTask(OpenRunningTaskPageMessage obj)
         {
             MainWindowFrame.NavigationService.Navigate(RunningTaskPage);
+        }
+
+        private void OpenOverviewTasks(OpenOverviewTaskPageMessage obj)
+        {
+            MainWindowFrame.NavigationService.Navigate(OverviewTasksPage);
         }
     }
 }
